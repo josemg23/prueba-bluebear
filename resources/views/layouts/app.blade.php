ab61e57066d0fb8c5fd7219b17e388a18597f8e6
JGMS <josegri_magallanes@hotmail.com>
2023-07-08 17:35:16 -0500

    titulo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Blue Bear') }}</title>
+    <title>Blue Bear</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
